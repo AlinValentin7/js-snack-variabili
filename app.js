296397ea9a97749ab24a1ee14c25026853e67119
AlinValentin7 <trandafiralionvalentin@gmail.com>
2024-06-28 11:00:32 +0200

    add:index.html-and-app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+//Esercizio 1
+
+//Dichiara una variabile chiamata number 
+let number;
+ //assegnale il valore 100.
+ number = 100 //number
+ //Stampa il valore della variabile in console.
+console.log(number) // fuction
+
+//Esercizio 2
+//Dichiara una costante chiamata PI | assegnale il valore di 3.14.
+const PI = 3.14 //number
+//Stampa il valore della costante in console.
+console.log(PI) // function
+
+//Esercizio 2/B
+//Prova ad assegnare alla costante chiamata PI il valore di 3.1416.
+//PI=3.1416 // number 
+//Sai dire cosa succede?
+/* Assegnando un secodno valore diverso dal primo a PI il JS si interrompe perche
+PI e stato dichiarato con "const" che è una costante, cioe il valore inserito non puo 
+essere cambiato nel tempo  */
+
+//Esercizio 2/C
+//Dichiara una variabile chiamata radius
+let radius;
+// assegnale il valore 8.
+radius = 8 // bumber
+// dichiara una variabile di nome circle
+let circle;
+//Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+circle = radius * 2 * PI //numebr
+//Stampa il valore della variabile circle in console.
+console.log(circle) //funcion 
+
+//Esercizio 3
+//Dichiara una variabile chiamata name 
+let name;
+//assegnale il valore “Carlo”.
+name = "Carlo" //string
+//Successivamente, modifica il valore della variabile in “Marco”.
+name = prompt("insercisci il tuo nome") //function 
+//Stampa il nuovo valore in console.
+console.log(name) //function 
+
